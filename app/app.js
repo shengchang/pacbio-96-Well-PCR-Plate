f5ab8af45ad43b3c505b1262bd1603c924174873
Cyril Gorrieri <cyril.gorrieri@gmail.com>
2016-06-21 14:16:58 -0700

    Added functionality and a better layout for the design

--- app/app.js
+++ app/app.js
@@ -8,7 +8,35 @@ function Well(letter, column) {
   this.column = column;
 }
 
+var ColorManager = function() {
+  var colors = {};
+
+  var letters = '0123456789ABCDEF'.split('');
+  var generateColor = function() {
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  return {
+    getColor: function(key) {
+      if(colors.hasOwnProperty(key)) return colors[key];
+      var color = generateColor();
+      colors[key] = color;
+      return color;
+    }
+  }
+};
+
 myApp.controller("main", ["$scope", function($scope) {
+  var colorManager = ColorManager();
+
+  $scope.getColor = function(well) {
+    if(!well.name) return "#fff";
+    return colorManager.getColor(well.name);
+  };
 
   $scope.form = {
     letters: ['A','B','C','D','E','F','G','H'],
@@ -29,12 +57,28 @@ myApp.controller("main", ["$scope", function($scope) {
   });
   $scope.plate = plate;
 
+  /**
+   * Get the well and set it in the form
+   * @param well
+   */
   $scope.getWell = function(well) {
     $scope.editedWell = angular.copy(well);
   };
 
+  /**
+   * Save the well defined in the for at the right location in the plate
+   */
   $scope.setWell = function() {
     $scope.plate[$scope.editedWell.letter][$scope.editedWell.column] = $scope.editedWell;
+  };
+
+  /**
+   * Clear and save the well at the plate location
+   */
+  $scope.clearWell = function() {
+    $scope.editedWell.time = undefined;
+    $scope.editedWell.name = undefined;
+    $scope.setWell();
   }
   
 }]);
